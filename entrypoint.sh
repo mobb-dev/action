77b7869f9f4477e30ab7f013713f08a3115f1e19
Jonathan Afek <jonathan@mobb.dev>
2023-07-12 17:33:29 +0300

    add npm debug prints

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 npm config set unsafe-perm true
 echo npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
 npx mobbdev@latest analyze --help
-npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
+DEBUG=* npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
