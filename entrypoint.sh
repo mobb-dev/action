187bc8d6b50dfe94175404fda4d4f4cdf79667fe
Jonathan Afek <jonathan@mobb.dev>
2023-07-12 17:36:36 +0300

    add --yes to npx

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ repository=$(git remote get-url origin)
 repository=${repository%".git"}
 branch=$(git rev-parse --abbrev-ref HEAD)
 npm config set unsafe-perm true
-echo npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
-npx mobbdev@latest analyze --help
-DEBUG=* npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
+echo npx --yes mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
+npx --yes mobbdev@latest analyze --help
+DEBUG=* npx --yes mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
