99877c0078143f15e94df33c501fdffac5c1ebad
Jonathan Afek <jonathan@mobb.dev>
2023-07-12 17:27:02 +0300

    debug entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 repository=$(git remote get-url origin)
 repository=${repository%".git"}
 branch=$(git rev-parse --abbrev-ref HEAD)
+echo npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
 npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
