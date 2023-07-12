c05a5e77e55eab1464b8120c7215c61f4e5083a0
Jonathan Afek <jonathan@mobb.dev>
2023-07-12 17:30:36 +0300

    unsafe perm

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 repository=$(git remote get-url origin)
 repository=${repository%".git"}
 branch=$(git rev-parse --abbrev-ref HEAD)
+npm config set unsafe-perm true
 echo npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
 npx mobbdev@latest analyze -y -r $repository -b $branch -k "$1" -f "$2"
